15eb6ae314e6d0105b5c477510ca74be6b500485
Genesis Ijeomah <gijeomah@my.waketech.edu>
2026-04-17 12:44:14 -0400

    code

---
+++ script.js
@@ -0,0 +1,19 @@
+// Access all the information 
+const sbtn = document.getElementById("sbtn")
+sbtn.addEventListner('click',function(){
+    const width = document.getElementById("width").value();
+    const height = document.getElementById("height").value();
+    
+});
+
+
+
+
+console.log(width);
+class Grid{
+    constructor(height, width){
+        //idk
+    }
+
+}
+//q.push(o), q.shift()-> returns 0 
