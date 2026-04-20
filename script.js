c3d60fad246e49b1cef4219d4329b2a12ce60989
gijeomah <gijeomah@my.waketech.edu>
2026-04-20 12:45:23 -0400

    code

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 // Access all the information 
-const sbtn = document.getElementById("sbtn")
-sbtn.addEventListner('click',function(){
-    const width = document.getElementById("width").value();
-    const height = document.getElementById("height").value();
+const sbtn = document.getElementById("sbtn");
+const widthElement = document.getElementById("width");
+const heightElement = document.getElementById("height");
+const srtxElement = document.getElementById("inpt_strt_x");
+const srtyElement = document.getElementById("inpt_strt_y");
+const endxElement = document.getElementById("inpt_end_x");
+const endyElement = document.getElementById("inpt_end_y");
+const tbl = document.getElementById("tbl");
+
+
+sbtn.addEventListener('click',function(){
+    //Obtains all values
+    width_val = widthElement.value; 
+    height_val = heightElement.value; 
+    srtx_val = srtxElement.value;
+    srty_val = srtyElement.value;
+    endx_val = endxElement.value;
+    endy_val = endyElement.value;
     
 });
 
-
-
-
-console.log(width);
 class Grid{
-    constructor(height, width){
-        //idk
-    }
-
-}
+    constructor(height, width,node){
+        ///code
+        height=height;
+        width=width;
+        node=node;
+        let NodesGrid =[];
+        for (let i = 0; i<=height; i++){
+            NodesGrid.push([]);
+           let row=document.createElement(tr);
+            tbl.appendChild(row);
+            for (let i = 0; i<= width; i++){
+                NodesGrid[i].push(node);
+               let tile=document.createElement(td);
+               row.appendChild(tile);
+            };
+        };
+    };
+    
+};
+Grid(height_val, width_val, 3);
 //q.push(o), q.shift()-> returns 0 
